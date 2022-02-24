fe292c6e5f87f94ddd7277d0fda3d5016de8d8de
k1jeong <kjeong522@gmail.com>
2022-02-24 22:34:07 +0900

    Extension으로 코드 관리

---
+++ Extension_practice/Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by Kwon Young Jeong on 2022/02/24.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIViewController {
+	
+	
+	func sayHi() {
+		print("Hi!")
+	}
+	
+	
+}
